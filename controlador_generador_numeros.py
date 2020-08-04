8ff71da8dbda7f984bce6dfb55f6677f9da9730b
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2020-08-04 12:37:26 -0300

    Agrego clase con metodos generadores de numeros aleatorios por metodos de congruente mixto y multiplcativo

---
+++ controlador_generador_numeros.py
@@ -0,0 +1,22 @@
+
+class ControladorGeneradorNumeros:
+
+    def generador_congruente_mixo(self, cantidad, semilla, a, c, m):
+        numeros_generados = [None] * cantidad
+        index = 0
+        for i in range(0, cantidad):
+            if index != 0:
+                semilla = (a * semilla + c) % m
+            numeros_generados[i] = semilla / m
+            index += 1
+        return numeros_generados
+
+    def generador_congruente_multiplicativo(self, cantidad, semilla, a, m):
+        numeros_generados = [None] * cantidad
+        index = 0
+        for i in range(0, cantidad):
+            if index != 0:
+                semilla = (a * semilla) % m
+            numeros_generados[i] = semilla / m
+            index += 1
+        return numeros_generados
