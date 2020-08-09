9c34b5f3d4ca2e2cde24a4722fe6e9742a4f600c
MMariatti <42209156+MMariatti@users.noreply.github.com>
2020-08-09 20:27:42 -0300

    agrego generador de intervalos

--- generador_numeros/dominio/controlador_generador_numeros.py
+++ generador_numeros/dominio/controlador_generador_numeros.py
@@ -63,3 +63,27 @@ class ControladorGeneradorNumeros:
     def generador_provisto_por_lenguaje(self, cantidad):
 
         return []
+# metodo para obtener los intervalos necesarios para realizar el grafico de frecuencia
+
+    def intervalos_chi_cuadrado(self, cantidad_Intervalos):
+    # El valor maximo del paso es 1 y el menor 0 ya que estamos trabajando con numeros aleatorios comprendidos entre 1 y 0
+        minimo = 0
+        maximo = 1
+        paso = (maximo - minimo)/cantidad_Intervalos
+        intervalos = []
+        media = []
+        i = 0
+        while i < cantidad_Intervalos :
+# el array de intervalos, termina siendo una matriz, ya que en la primer columna se guarda el valor de piso del intevalo y en la segunda se guarda el valor de techo del intervalo
+            if i == 0:
+                intervalos.append([round(minimo,4),round(minimo + paso, 4)] )
+            else:
+                ultimoMinimo = round(intervalos[i-1][1],4)
+                intervalos.append([ultimoMinimo,round(ultimoMinimo+paso,4)])
+
+            i=+1
+
+        for i in intervalos:
+            media.append(round((i[0] + i[1]) / 2, 4))
+
+        return intervalos, media
