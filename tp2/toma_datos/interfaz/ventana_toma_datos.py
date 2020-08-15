fb2254911a4020a78871c9d0004e717dbb214770
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2020-08-15 19:23:40 -0300

    Desarrollo interfaz grafica

--- tp2/toma_datos/interfaz/ventana_toma_datos.py
+++ tp2/toma_datos/interfaz/ventana_toma_datos.py
@@ -5,7 +5,8 @@ from soporte.ruta import Ruta
 
 class VentanaTomaDatos(QMainWindow):
 
-	def __init__(self):
-		# Genero ventana a partir de ui y creo controlador
-		QMainWindow.__init__(self)
-		uic.loadUi(Ruta.generar_ruta("interfaz/ventana_toma_datos.ui"), self)
+    def __init__(self):
+
+        # Genero ventana a partir de ui y creo controlador
+        QMainWindow.__init__(self)
+        uic.loadUi(Ruta.generar_ruta("interfaz/ventana_toma_datos.ui"), self)
