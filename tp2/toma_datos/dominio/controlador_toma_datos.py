64a891b83d764aeea8c342181667dc510883a94b
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2020-08-18 01:09:13 -0300

    Desarrollo toma de datos desde xlsx

--- tp2/toma_datos/dominio/controlador_toma_datos.py
+++ tp2/toma_datos/dominio/controlador_toma_datos.py
@@ -1,8 +1,30 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import xlrd
 
 
 class ControladorTomaDatos:
+
+    def obtener_variables_aleatorias(self, ruta_archivo, columna, fila_desde, fila_hasta):
+
+        # Adapto parametros para usarlos con libreria
+        columna = columna - 1
+        fila_desde = fila_desde - 1
+        fila_hasta = fila_hasta - 1
+
+        # Inicializo lista
+        variables_aleatorias = []
+
+        # Obtengo planilla
+        file = xlrd.open_workbook(ruta_archivo)
+        hoja = file.sheet_by_index(0)
+
+        # Guardo valores en lista
+        for i in range(fila_desde, fila_hasta + 1):
+            valor = round(float(hoja.cell_value(i, columna)), 4)
+            if valor == int(valor):
+                valor = int(valor)
+            variables_aleatorias.append(valor)
+
+        return variables_aleatorias
 
     def calcular_frecuencias_por_intervalo(self, variables_aleatorias, cantidad_intervalos):
         pass
--- tp2/toma_datos/interfaz/ventana_toma_datos.py
+++ tp2/toma_datos/interfaz/ventana_toma_datos.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from os.path import expanduser
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QFileDialog
 from PyQt5 import uic
 from dominio.controlador_toma_datos import ControladorTomaDatos
 from soporte.validador_enteros import ValidadorEnteros
@@ -10,6 +11,7 @@ class VentanaTomaDatos(QMainWindow):
     """ Atributos """
 
     controlador = None
+    ruta_archivo = None
     variables_aleatorias = []
 
     """ Constructor """
@@ -22,13 +24,14 @@ class VentanaTomaDatos(QMainWindow):
         self.controlador = ControladorTomaDatos()
 
         # Agrego validadores a los campos
-        validador_enteros = ValidadorEnteros(2)
+        validador_enteros = ValidadorEnteros(9)
         self.txt_columna.setValidator(validador_enteros)
         self.txt_fila_desde.setValidator(validador_enteros)
         self.txt_fila_hasta.setValidator(validador_enteros)
 
         # Conecto los botones con los eventos
         self.btn_seleccionar_archivo.clicked.connect(self.accion_seleccionar_archivo)
+        self.btn_obtener_datos.clicked.connect(self.accion_obtener_datos)
         self.btn_generar_histograma.clicked.connect(self.accion_generar_histograma)
         self.btn_prueba_1.clicked.connect(self.accion_prueba_1)  # TODO: No definido
         self.btn_prueba_1.clicked.connect(self.accion_prueba_2)  # TODO: No definido
@@ -36,7 +39,40 @@ class VentanaTomaDatos(QMainWindow):
     """ Acciones """
 
     def accion_seleccionar_archivo(self):
-        pass
+
+        # Obtengo ruta del xlxs
+        self.ruta_archivo = QFileDialog.getOpenFileName(self, "Seleccione su archivo de datos", expanduser("~"),
+                                                        "Excel (*.xls *.xlsx)")[0]
+
+    def accion_obtener_datos(self):
+
+        # Obtengo y valido datos
+        if self.ruta_archivo is None or self.ruta_archivo == "":
+            self.mostrar_mensaje("Error", "Debe seleccionar un archivo")
+            return
+        columna = int(self.txt_columna.text())
+        if columna == "" or int(columna <= 0):
+            self.mostrar_mensaje("Error", "El número de columna de donde tomar los datos debe ser mayor a cero")
+            return
+        fila_desde = int(self.txt_fila_desde.text())
+        if fila_desde == "" or int(fila_desde <= 0):
+            self.mostrar_mensaje("Error", "El número de fila desde donde tomar los datos debe ser mayor a cero")
+            return
+        fila_hasta = int(self.txt_fila_hasta.text())
+        if fila_hasta == ""or int(fila_hasta <= 0):
+            self.mostrar_mensaje("Error", "El número de fila hasta donde tomar los datos debe ser mayor a cero")
+            return
+
+        # Obtengo variables aleatorias desde archivo
+        try:
+            self.variables_aleatorias = self.controlador.obtener_variables_aleatorias(self.ruta_archivo, columna,
+                                                                                      fila_desde, fila_hasta)
+            self.mostrar_mensaje("Valores obtenidos", "Se obtuvieron correctamente %s valores" %
+                                 len(self.variables_aleatorias))
+
+        except Exception as ex:
+            self.mostrar_mensaje("Error", "Hubo un problema al obtener los datos del archivo")
+            print(ex)
 
     def accion_generar_histograma(self):
 
