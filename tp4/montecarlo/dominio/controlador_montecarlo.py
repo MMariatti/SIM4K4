6b59c0758e4c18645d7efb59d511380d68f5acf9
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2020-09-14 10:14:37 -0300

    Agrego ingreso y beneficio diario

--- tp4/montecarlo/dominio/controlador_montecarlo.py
+++ tp4/montecarlo/dominio/controlador_montecarlo.py
@@ -65,17 +65,18 @@ class ControladorMontecarlo:
         frascos_disponibles = 0
         frascos_desechos = 0
         cafe_disponible = 0
-        cafe_faltante = 0
 
         # Bucle principal
         for dia in range(1, dias + 1):
 
             # Actualizo frascos disponibles o descuento dia de demora al iniciar el dia
+            costo = 0
             if demora is not None:
                 if demora != 0:
                     demora -= 1
                 if demora == 0:
                     demora = None
+                    costo = frascos_a_comprar * 250
                     frascos_llegados = frascos_a_comprar
                     frascos_disponibles += frascos_llegados
 
@@ -102,13 +103,23 @@ class ControladorMontecarlo:
             demanda_tarde = generador_exponencial.generar_numero_aleatorio()
             demanda = demanda_maniana + demanda_tarde
 
-            # Actualizo los gramos cafe disponible al finalizar el dia y calculo la demanda no abastecida
+            # Calculo la demanda abastecida y no abastecida y actualizo los gramos de cafe disponibles al finalizar
+            # el dia
+            demanda_abastecida = 0
             demanda_no_abastecida = 0
             if demanda <= cafe_disponible:
+                demanda_abastecida = demanda
                 cafe_disponible -= demanda
             else:
+                demanda_abastecida = cafe_disponible
                 demanda_no_abastecida = demanda - cafe_disponible
                 cafe_disponible = 0
+
+            # Ingreso diario
+            ingreso = demanda_abastecida * 1.5
+
+            # Beneficio diario
+            beneficio = ingreso - costo
 
             # Actualizo los frascos disponibles al finalizar el dia
             frascos_disponibles = math.ceil(cafe_disponible / frascos_disponibles)
@@ -128,8 +139,12 @@ class ControladorMontecarlo:
                 "cafe_disponible": cafe_disponible,
                 # "cafe_disponible_promedio": cafe_disponible_promedio,
                 "demanda": demanda,
-                "demanda_no_abastecida": demanda_no_abastecida
-                # "demanda_no_abastecida_promedio": demanda_no_abastecida_promedio
+                "demanda_no_abastecida": demanda_no_abastecida,
+                # "demanda_no_abastecida_promedio": demanda_no_abastecida_promedio,
+                "ingreso": ingreso,
+                # "ingreso_promedio": ingreso_promedio,
+                "beneficio": beneficio,
+                # "beneficio_promedio": beneficio_promedio
             })
 
         """
@@ -158,106 +173,3 @@ class ControladorMontecarlo:
         """
 
         return [], {}
-
-    """
-    def simular_montecarlo(self, cantidad_dias, capacidad_maxima=10):
-
-        demanda_manana = 0
-        beneficio_manana = 0
-        beneficio_tarde = 0
-        faltante_manana = 0
-        faltante_tarde = 0
-        # demora = 0
-        # frascos_regalados = 0
-        # frascos_disponibles = 2
-
-        faltante_total = 0
-        simulacion_montecarlo = []* cantidad_dias
-
-        for sim in range(len(simulacion_montecarlo)):
-
-        # Planteo  condiciones de entrega y demora de pedidos
-
-            # random_demora = round(random.random(), 4)
-
-            # if sim % 2 == 0 and 0 <= random_demora < 0.5:
-            #     demora = 0
-            #     frascos_disponibles += 2
-            # elif sim % 2 == 0 and 0.5 <= random_demora < 0.75:
-            #     demora = 1
-            # elif sim % 2 == 0 and 0.75 <= random_demora < 1:
-            #     demora = 2
-            #
-            # elif sim % 2 != 0:
-            #     random_demora = "X"
-            #     demora = demora - 1
-            #     if demora == 0:
-            #         frascos_disponibles += 2
-            #
-            # if frascos_disponibles > capacidad_maxima:
-            #     frascos_regalados += (frascos_disponibles-capacidad_maxima)
-            #     frascos_disponibles = capacidad_maxima
-
-            # cafe_disponible = frascos_disponibles*170
-
-            # Planteo condiciones del turno mañana
-
-            # random_manana = round(random.random(), 4)
-            # if random_manana < 0.5:
-            #
-            #     demanda_manana = 50
-            # 
-            # elif 0.5 <= random_manana < 1:
-            #
-            #     demanda_manana = round(random.normalvariate(75, 15), 4)
-            
-            # if demanda_manana <= cafe_disponible:
-
-                beneficio_manana = round((demanda_manana * 15), 4)
-            #     cafe_disponible = cafe_disponible-demanda_manana
-
-            # elif demanda_manana > cafe_disponible:
-
-                beneficio_manana = round((cafe_disponible*15), 4)
-            #     faltante_manana = round((demanda_manana-cafe_disponible), 4)
-            #     cafe_disponible = 0
-
-            # Planteo condiciones del turno tarde
-
-            # demanda_tarde = round(np.random.exponential(79), 0)
-            #
-            # if demanda_tarde <= cafe_disponible:
-            #
-            #     beneficio_tarde = round((demanda_tarde*15), 4)
-            #     cafe_disponible = round((cafe_disponible-demanda_tarde), 4)
-            #
-            # elif demanda_tarde > cafe_disponible:
-                beneficio_tarde = round((cafe_disponible*15), 4)
-            #     faltante_tarde = round((demanda_tarde-cafe_disponible), 4)
-            #     cafe_disponible = 0
-
-            # if cafe_disponible == 0:
-                beneficio_tarde = 0
-
-            # demanda_total = round((demanda_manana + demanda_tarde), 4)
-
-            faltante_total = round((faltante_manana+faltante_tarde), 4)
-
-            beneficio_total = round((beneficio_manana + beneficio_tarde), 4)
-
-            # simulacion_montecarlo.append({"Dia":sim+1,
-            #                               "Random demora":random_demora,
-            #                               "Demora":demora,
-            #                               "Frascos disponibles":frascos_disponibles,
-                                          "Random mañana":random_manana,
-                                          "Mañana":demanda_manana,
-                                          "Beneficio mañana":beneficio_manana,
-                                          "Faltante mañana":faltante_manana
-                                          "Tarde":demanda_tarde,
-                                          "Beneficio tarde":beneficio_tarde
-                                          "Faltante tarde":faltante_tarde,
-                                          "Beneficio":beneficio_total
-                                          "Faltante":faltante_total,
-                                          "Frascos regalados":frascos_regalados
-                                        })
-    """
